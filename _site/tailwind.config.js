1283d66ba8f1430816af48d1a5251adcc1dfe4bd
huruifeng <huruifeng.cn@hotmail.com>
2025-02-22 10:59:49 -0500

    update

---
+++ _site/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './_site/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './*.md',
+    './*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
